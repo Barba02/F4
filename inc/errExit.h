32074adf609854a05fd8540ae1a806460dc0499b
Filippo Barbieri <pippobarba02@gmail.com>
2023-05-04 23:47:16 +0100

    Comments added

--- inc/errExit.h
+++ inc/errExit.h
@@ -1,6 +1,7 @@
 #ifndef _ERREXIT_HH
 #define _ERREXIT_HH
 
+// error exit function signature
 void errExit(const char *msg);
 
 #endif
--- src/errExit.c
+++ src/errExit.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 void errExit(const char *msg) {
+    // print error message passed as argument
     perror(msg);
+    // exit the program with bad exit
     exit(1);
 }
