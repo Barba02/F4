f04bcd9629966190442603bfbc0f8f77906ef507
Barba02 <69804640+Barba02@users.noreply.github.com>
2023-06-15 15:10:04 +0100

    add semaphores lib

---
+++ inc/semaphores.h
@@ -0,0 +1,18 @@
+#ifndef _SEMAPHORES_HH
+#define _SEMAPHORES_HH
+
+#include <stdlib.h>
+
+union semun {
+    int val;
+    struct semi_ds *buf;
+    unsigned short *array;
+};
+
+void semOp(int semid, unsigned short sem_num, short sem_op);
+
+int create_sem_set(key_t semkey, int nsems, unsigned short *values);
+
+void remove_sem_set(int semid);
+
+#endif
