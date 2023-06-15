fdb4ba6314499084ffa1c53cc3dcf11cfe3669b5
Barba02 <69804640+Barba02@users.noreply.github.com>
2023-06-15 15:00:09 +0100

    optional IPC_EXCL

---
+++ F4Client.c
@@ -0,0 +1,95 @@
+#include <time.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
+#include "shared_memory.h"
+#include "errExit.h"
+#include "game.h"
+
+int shmid_data; // shared segment's id for game data
+game_t* game_data; // shared struct containing game data
+int shmid_matrix; // shared segment's id for game matrix
+int (*game_matrix)[]; // shared matrix to play
+
+// catches SIGUSR1
+void sigUsr1Handler(int sig) {
+    printf("Other player found\n");
+    // start game
+    F4_game(game_data, game_matrix);
+}
+
+// catches SIGTERM
+void sigTermHandler(int sig) {
+    printf("\nGAME OVER : ");
+    if(game_data->n_played == game_data->rows*game_data->cols)
+        printf("DRAW\n");
+    else
+        printf("%s WIN!\n",(game_data->last_player == 1)? game_data->client1_username : game_data->client2_username);
+    exit(0);
+}
+
+int main (int argc, char *argv[]) {
+    
+
+    // setting SIGUSR1 handling
+    if (signal(SIGUSR1, sigUsr1Handler) == SIG_ERR)
+        errExit("Cannot change signal handler");
+
+    // setting SIGUSR1 handling
+    if (signal(SIGTERM, sigTermHandler) == SIG_ERR)
+        errExit("Cannot change signal handler");
+
+    // check command line arguments number
+    if (argc < 2 || argc > 3) {
+        printf("Usage: %s <username> [autoPlay]\n", argv[0]);
+        return 1;
+    }
+    // validation of the arguments
+    else {
+        game_data->autoplay = (argc == 3 && strcmp(argv[2], "1") == 0) ? 1 : 0;
+    }
+
+    // attach to game data
+    shmid_data = alloc_shared_memory(sizeof(game_t), GAME_KEY, 0);
+    game_data = (game_t*) get_shared_memory(shmid_data);
+
+    // attach to game matrix
+    shmid_matrix = alloc_shared_memory(sizeof(int[game_data->rows][game_data->cols]),MATRIX_KEY, 0);
+    game_matrix = get_shared_memory(shmid_matrix);
+
+    // check if this client is user 1 or 2
+    if (game_data->client1_pid == -1) {
+        game_data->client1_pid = getpid();
+        strcpy(game_data->client1_username,argv[1]);
+    }
+    else {
+        game_data->client2_pid = getpid();
+        strcpy(game_data->client2_username,argv[1]);
+    }
+
+    //notify server (check if is the first or second client)
+    if (getpid() == game_data->client1_pid) {
+        kill(game_data->server_pid, SIGUSR1);
+        //first player wait for second player
+        printf("Waiting for another player...\n");
+        fflush(stdin);
+    }
+    else
+        kill(game_data->server_pid,SIGUSR2);
+    
+    while(1)
+    {
+        print_game(game_data->rows,game_data->cols,game_matrix,game_data->client1_sign,game_data->client2_sign);
+
+        F4_game(game_data,game_matrix);
+    }
+        //TODO: gioco automatico con un bot
+    
+    
+    return 0;
+}
---
+++ shared_memory.h
@@ -0,0 +1,32 @@
+#ifndef _SHARED_MEMORY_HH
+#define _SHARED_MEMORY_HH
+
+#include <stdlib.h>
+
+#define GAME_KEY 67890
+#define MATRIX_KEY 54321
+
+typedef struct game {
+    pid_t server_pid;
+    pid_t client1_pid;
+    pid_t client2_pid;
+    int last_player;
+    int rows;
+    int cols;
+    int autoplay;
+    char client1_sign;
+    char client2_sign;
+    char client1_username[25];
+    char client2_username[25];
+    int n_played;
+} game_t;
+
+int alloc_shared_memory(size_t size, key_t key, int ipc_excl);
+
+void *get_shared_memory(int shmid);
+
+void free_shared_memory(void *ptr_sh);
+
+void remove_shared_memory(int shmid);
+
+#endif
